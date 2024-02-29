7dd0d021fabe853cb2ef9686b52f9f7e3967e5af
sboczek <sboczek@pixeledgegames.com>
2024-02-29 13:33:02 +0100

    sticker textures SO

---
+++ Scripts/ScriptableObjects/PTK_StickerTexures.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StickerTextures", menuName = "PixelTools/StickerTexturesSO", order = 0)]
+public class PTK_StickerTexures : ScriptableObject
+{
+    public Texture2D textureClamp;
+    public Texture2D textureRepeat;
+}
