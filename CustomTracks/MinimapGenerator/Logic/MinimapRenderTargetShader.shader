d33669916644cbab2c41bd173dfc6698a345e52d
Sboczek <sboczek@pixeledgegames.com>
2024-03-18 11:32:13 +0100

    minimap generator

---
+++ CustomTracks/MinimapGenerator/Logic/MinimapRenderTargetShader.shader
@@ -0,0 +1,52 @@
+Shader "Unlit/MinimapRenderTargetShader"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float4 worldPos : TEXCOORD1;
+            };
+
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = i.worldPos + fixed4(0,5000,0,1); // + 5000 to see the difference between position in model and no model in scene ad all ( 0 )
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
--- CustomTracks/MinimapGenerator/Logic/PTK_MinimapRenderAndSave.cs
+++ CustomTracks/MinimapGenerator/Logic/PTK_MinimapRenderAndSave.cs
@@ -7,17 +7,34 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class PTK_MinimapRenderAndSave : MonoBehaviour
 {
+#if UNITY_EDITOR
     [Header("Logic Setup")]
     public PTK_MinimapRender_CornerPosCalc minimapCornerCalc;
     public Camera renderTexCamera;
+    public RenderTexture colorRenderTexture;
+    public RenderTexture edgeRenderTexture;
+    public UnityEngine.UI.Image generatedPreviewImage;
+
+    public Shader renderEdgesShader;
     public GameObject models3DParent;
+
+    [Header("Finish line")]
+    public Transform finishLineTransform;
+    public int iFinishLineCellSizeInPixels = 16;
+    public int iFinishLineWidthCellCount = 4;
 
     [Header("Edit Camera Pos & Size")]
     [Range(0,2)]
     public float fEnviroSizeInTexture = 1.0f;
     public Vector2 cameraOffsetForRender;
     [Header("Last Generated Minimap")]
+    public string strLastMinimapGeneratedDirPath;
     public Sprite lastGeneratedMinimapSprite;
+
+
+
+#endif
+
     private void Awake()
     {
         if(Application.isPlaying)
@@ -46,32 +63,47 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         }
     }
 
+    string directoryPath = "";
+
     [EasyButtons.Button]
     void RenderMinimapAndSaveFileInChoosenDirectory()
     {
+        renderTexCamera.targetTexture = colorRenderTexture;
         renderTexCamera.Render();
-        string directoryPath = UnityEditor.EditorPrefs.GetString("MinimapLastSaveDir");
-        directoryPath = ( UnityEditor.EditorUtility.OpenFolderPanel("Save texture as PNG", directoryPath,""));
-        UnityEditor.EditorPrefs.SetString("MinimapLastSaveDir", directoryPath);
 
+        renderTexCamera.targetTexture = edgeRenderTexture;
+        renderTexCamera.RenderWithShader(renderEdgesShader,"");
+
+        renderTexCamera.targetTexture = colorRenderTexture;
+
+        strLastMinimapGeneratedDirPath = ( UnityEditor.EditorUtility.OpenFolderPanel("Save texture as PNG", strLastMinimapGeneratedDirPath, ""));
+        directoryPath = strLastMinimapGeneratedDirPath;
 
         if (!string.IsNullOrEmpty(directoryPath))
         {
             bool bCanCreateNewMinimap = DeleteExistingMinimapFiles(directoryPath);
 
             if(bCanCreateNewMinimap == true)
-                SaveRenderTextureToPNG(renderTexCamera.targetTexture, directoryPath);
+                SaveRenderTextureToPNG( directoryPath);
         }
     }
 
-    void SaveRenderTextureToPNG(RenderTexture renderTexture, string directoryPath)
+    void SaveRenderTextureToPNG( string directoryPath)
     {
         minimapCornerCalc.RefreshCornerPos();
 
-        RenderTexture.active = renderTexture;
-        Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false,false);
-        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        // color tex
+        RenderTexture.active = colorRenderTexture;
+        Texture2D texture2D = new Texture2D(colorRenderTexture.width, colorRenderTexture.height, TextureFormat.RGBA32, false,false);
+        texture2D.ReadPixels(new Rect(0, 0, colorRenderTexture.width, colorRenderTexture.height), 0, 0);
         texture2D.Apply();
+
+        // edge tex
+        RenderTexture.active = edgeRenderTexture;
+        Texture2D edgeTexture2D = new Texture2D(edgeRenderTexture.width, edgeRenderTexture.height, TextureFormat.RGBAFloat, false, false);
+        edgeTexture2D.ReadPixels(new Rect(0, 0, edgeRenderTexture.width, edgeRenderTexture.height), 0, 0);
+        edgeTexture2D.Apply();
+
 
         Color[] pixels = texture2D.GetPixels();
         for (int i = 0; i < pixels.Length; i++)
@@ -82,8 +114,10 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         texture2D.Apply();
 
         Color32 edgeColor = new Color32(176, 110, 73, 255);
-        DetectAndDrawEdges(texture2D, 16, edgeColor);
+        DetectAndDrawEdges(texture2D, edgeTexture2D, 20, edgeColor);
         SmoothTransition(texture2D, edgeColor);
+
+        DrawFinishLine(texture2D, finishLineTransform, iFinishLineCellSizeInPixels, iFinishLineCellSizeInPixels * iFinishLineWidthCellCount, 2);
 
         string filename = $"minimap_blX_{minimapCornerCalc.bl_CornerWorldPos.x}_blY_{minimapCornerCalc.bl_CornerWorldPos.y}_trX_{minimapCornerCalc.tr_CornerWorldPos.x}_trY_{minimapCornerCalc.tr_CornerWorldPos.y}.png";
 
@@ -117,6 +151,7 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
             }
 
             lastGeneratedMinimapSprite = UnityEditor.AssetDatabase.LoadAssetAtPath<Sprite>(strTexPathFromAssets);
+            generatedPreviewImage.sprite = lastGeneratedMinimapSprite;
 
             EditOrCreateMinimapSO(directoryPath, lastGeneratedMinimapSprite, minimapCornerCalc.bl_CornerWorldPos, minimapCornerCalc.tr_CornerWorldPos);
         }
@@ -124,12 +159,22 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         Debug.Log($"Minimap saved to: {fullPathMinimap}");
     }
 
-    public static void DetectAndDrawEdges(Texture2D texture, int edgeLineWidth, Color edgeColor)
+    public  void DetectAndDrawEdges(Texture2D texture, Texture2D edgeTexture, int edgeLineWidth, Color edgeColor)
     {
         Color[] pixels = texture.GetPixels();
         int width = texture.width;
         int height = texture.height;
         Color[] newPixels = new Color[pixels.Length];
+
+        var rawData = edgeTexture.GetRawTextureData<float>();
+        float[] heightAtPixelColor = new float[edgeTexture.width * edgeTexture.height];
+        int iHeightIndex = 0;
+
+        for (int i = 0; i < rawData.Length; i += 4)
+        {
+            heightAtPixelColor[iHeightIndex] = rawData[i + 1];
+            iHeightIndex++;
+        }
 
         // Copy original pixels to newPixels to start modifications
         System.Array.Copy(pixels, newPixels, pixels.Length);
@@ -138,8 +183,10 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         {
             for (int x = 0; x < width; x++)
             {
+               
                 // Find edges based on alpha value
-                if (IsEdge(x, y, width, height, pixels))
+                //if (IsEdge(x, y, width, height, pixels))
+                if (IsEdgeHeightBased(x, y, width, height, heightAtPixelColor))
                 {
                     // Draw edge with specified line width and smooth transition
                     for (int dy = -edgeLineWidth; dy <= edgeLineWidth; dy++)
@@ -154,8 +201,12 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
                                 float distance = Mathf.Sqrt(dx * dx + dy * dy) / edgeLineWidth;
                                 distance = Mathf.Clamp01(distance);
 
+                                float fCurPixelHeight = heightAtPixelColor[newY * width + newX];
+                                float fEdgePixelHeight = heightAtPixelColor[y * width + x];
                                 // Only blend colors where the original pixel is transparent to preserve image content
-                                if (pixels[newY * width + newX].a < 1)
+                                bool bCanDrawEdgeOnPixel =  fCurPixelHeight == 0; // no track data here
+                                bCanDrawEdgeOnPixel = (fEdgePixelHeight - fCurPixelHeight) > fHeightDiffToDetectEdge; // edge heigth is higher then neightbour (if edge is above the track that is under then we want to draw it)
+                                if (bCanDrawEdgeOnPixel == true)
                                 {
                                     Color originalColor = newPixels[newY * width + newX];
                                     Color blendedColor = Color.Lerp(edgeColor, originalColor, distance);
@@ -173,7 +224,104 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         texture.Apply();
     }
 
-    private static bool IsEdge(int x, int y, int width, int height, Color[] pixels)
+    public void DrawFinishLine(Texture2D texture, Transform finishLineTransform, int cellSize, int finishLineWidthPixels, int iBorderLineWidth)
+    {
+        int finishLineHeightPixels = cellSize * 2; // Assuming 2 rows of checkers
+        Color[] pixels = texture.GetPixels();
+        int width = texture.width;
+        int height = texture.height;
+        Color[] newPixels = new Color[pixels.Length];
+
+        // Copy original pixels to newPixels to start modifications
+        System.Array.Copy(pixels, newPixels, pixels.Length);
+
+        // Center of the finish line in texture space
+        Vector2 center = WorldToTexture(finishLineTransform.position, texture);
+        // Direction of the finish line in texture space
+        Vector3 direction = finishLineTransform.forward; direction.y = 0.0f; direction.Normalize();
+
+        // Calculate the rotation angle from the direction
+        float angleRad = Vector3.Angle(direction, Vector3.forward) * Mathf.Deg2Rad;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // Convert current pixel position to texture space relative to center
+                Vector2 pos = new Vector2(x - center.x, y - center.y);
+                // Rotate the position to align with the finish line rotation
+                Vector2 rotatedPos = RotatePoint(pos, angleRad);
+
+                // Check if the pixel falls within the finish line width and height
+                if (Math.Abs(rotatedPos.x) <= finishLineWidthPixels / 2.0f && Math.Abs(rotatedPos.y) <= finishLineHeightPixels / 2.0f)
+                {
+                    if(rotatedPos.y  < 2 && rotatedPos.y > -2)
+                    {
+                        newPixels[y * width + x] = Color.black; // middle line
+                        continue;
+                    }
+
+                    // Check for border
+                    bool isBorder = Math.Abs(rotatedPos.x) >= (finishLineWidthPixels / 2.0f - iBorderLineWidth) || Math.Abs(rotatedPos.y) >= (finishLineHeightPixels / 2.0f - iBorderLineWidth);
+
+                    if (isBorder)
+                    {
+                        newPixels[y * width + x] = Color.black; // Border color
+                    }
+                    else
+                    {
+                        // Determine the cell coordinates within the checker pattern
+                        int cellX = (int)((rotatedPos.x + finishLineWidthPixels / 2.0f) / cellSize) % 2;
+                        int cellY = (int)((rotatedPos.y + finishLineHeightPixels / 2.0f) / cellSize) % 2;
+
+                        // Alternate colors based on cell coordinates
+                        if ((cellX + cellY) % 2 == 0)
+                        {
+                            newPixels[y * width + x] = Color.white;
+                        }
+                        else
+                        {
+                            newPixels[y * width + x] = Color.black;
+                        }
+                    }
+                }
+            }
+        }
+
+        texture.SetPixels(newPixels);
+        texture.Apply();
+    }
+    // Convert world position to texture space (Example implementation)
+    private Vector2 WorldToTexture(Vector3 worldPos, Texture2D texture)
+    {
+        // Normalize the world position to a value between 0 and 1
+        float normalizedX = (worldPos.x - minimapCornerCalc.bl_CornerWorldPos.x) / (minimapCornerCalc.tr_CornerWorldPos.x - minimapCornerCalc.bl_CornerWorldPos.x);
+        float normalizedY = (worldPos.y - minimapCornerCalc.bl_CornerWorldPos.y) / (minimapCornerCalc.tr_CornerWorldPos.y - minimapCornerCalc.bl_CornerWorldPos.y);
+
+        // Convert the normalized position to texture space
+        int textureX = Mathf.FloorToInt(normalizedX * texture.width);
+        int textureY = Mathf.FloorToInt(normalizedY * texture.height);
+
+        return new Vector2(textureX, textureY);
+    }
+
+
+    // Rotate a point by an angle in radians
+    private Vector2 RotatePoint(Vector2 point, float angle)
+    {
+        float cosAngle = Mathf.Cos(angle);
+        float sinAngle = Mathf.Sin(angle);
+        return new Vector2(
+            cosAngle * point.x - sinAngle * point.y,
+            sinAngle * point.x + cosAngle * point.y
+        );
+    }
+
+    /// <summary>
+    ///  finish line end
+    /// </summary>
+
+    private  bool IsEdge(int x, int y, int width, int height, Color[] pixels)
     {
         bool isCurrentPixelTransparent = pixels[y * width + x].a < 1;
         // Check surrounding pixels to determine if this is an edge
@@ -197,7 +345,38 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         return false;
     }
 
-    public static void SmoothTransition(Texture2D texture, Color edgeColor, float tolerance = 0.1f)
+    public float fHeightDiffToDetectEdge = 5.0f;
+    private bool IsEdgeHeightBased(int x, int y, int width, int height, float[] heights)
+    {
+        float currentPixelHeight = heights[y * width + x];
+
+        // Check surrounding pixels to determine if this is an edge
+        int[] dx = { 0, 1, 0, -1 };
+        int[] dy = { -1, 0, 1, 0 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if (newX >= 0 && newY >= 0 && newX < width && newY < height)
+            {
+                float fNeightbourHeight = heights[newY * width + newX];
+                if (currentPixelHeight < 1000 && fNeightbourHeight > 0) // this pixel is outside track
+                {
+                    return true; // Edge detected
+                }
+
+                if ((currentPixelHeight - fNeightbourHeight) > fHeightDiffToDetectEdge) // this pixel is outside track
+                {
+                    return true; // Edge detected
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public  void SmoothTransition(Texture2D texture, Color edgeColor, float tolerance = 0.1f)
     {
         Color[] pixels = texture.GetPixels();
         int width = texture.width;
@@ -238,7 +417,7 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         texture.Apply();
     }
 
-    private static bool ContainsEdgeColor(int x, int y, int width, int height, Color[] pixels, Color edgeColor, float tolerance)
+    private  bool ContainsEdgeColor(int x, int y, int width, int height, Color[] pixels, Color edgeColor, float tolerance)
     {
         for (int dy = -2; dy <= 1; dy++)
         {
@@ -258,13 +437,13 @@ public class PTK_MinimapRenderAndSave : MonoBehaviour
         return false;
     }
 
-    private static bool IsSimilarColor(Color color1, Color color2, float tolerance)
+    private  bool IsSimilarColor(Color color1, Color color2, float tolerance)
     {
         return Math.Abs(color1.r - color2.r) < tolerance && Math.Abs(color1.g - color2.g) < tolerance &&
                Math.Abs(color1.b - color2.b) < tolerance && Math.Abs(color1.a - color2.a) < tolerance;
     }
 
-    private static Color BlendWithNeighbors(int x, int y, int width, int height, Color[] pixels, Color edgeColor, float tolerance)
+    private  Color BlendWithNeighbors(int x, int y, int width, int height, Color[] pixels, Color edgeColor, float tolerance)
     {
         Color blendedColor = pixels[y * width + x];
         int count = 1; // Start with 1 to include the original pixel
