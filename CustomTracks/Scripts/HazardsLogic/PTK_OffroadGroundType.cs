b66d78f718899e433b9f77888256d9ddafc6c284
Sboczek <sboczek@pixeledgegames.com>
2024-08-06 10:19:41 +0200

    offroad ground logic - wip

---
+++ CustomTracks/Scripts/HazardsLogic/PTK_OffroadGroundType.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PTK_OffroadGroundType : MonoBehaviour
+{
+    public enum EOffroadGroundType
+    {
+        GRASS,
+        DIRT,
+        ICE,
+        SNOW,
+        LAVA
+    }
+    public float fGroundFriction = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
