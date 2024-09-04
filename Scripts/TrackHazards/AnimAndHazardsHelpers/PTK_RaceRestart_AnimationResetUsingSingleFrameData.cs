f2df2be5d88635e2c98eb481405ad86cc713f37b
Sboczek <sboczek@pixeledgegames.com>
2024-09-04 22:07:13 +0200

    removed obsolete classes

--- Scripts/TrackHazards/AnimAndHazardsHelpers/PTK_RaceRestart_AnimationResetUsingSingleFrameData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Obsolete("Script Deprecated. Please REMOVE IT and use PTK_RaceRestart_AnimationSync instead.")]
-[RequireComponent(typeof(PTK_RaceRestart_AnimationSync))]
-public class PTK_RaceRestart_AnimationResetUsingSingleFrameData : MonoBehaviour
-{
-}
