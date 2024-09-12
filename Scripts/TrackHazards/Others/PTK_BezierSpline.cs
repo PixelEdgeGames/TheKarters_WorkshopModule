9727635f8618fc3462164ebc25674cfb37f31d27
sboczek <sboczek@pixeledgegames.com>
2024-09-12 17:07:33 +0200

    bezier path sync to restart race

--- Scripts/TrackHazards/Others/PTK_BezierSpline.cs
+++ Scripts/TrackHazards/Others/PTK_BezierSpline.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,47 @@ public class PTK_BezierSpline : MonoBehaviour
     [HideInInspector]
     public CPC_BezierPath bezierPath;
 
+    bool bWasPlayOnAwake = false;
     // Start is called before the first frame update
     void Awake()
     {
         if (bezierPath == null)
             bezierPath = this.GetComponent<CPC_BezierPath>();
+
+        PTK_ModGameplayDataSync.Instance.gameEvents.OnGameEvent_RaceRestarted += RaceRestarted;
+        PTK_ModGameplayDataSync.Instance.gameEvents.OnGameEvent_RaceTimerStart += RaceTimerStart;
+
+        if (bezierPath != null)
+            bWasPlayOnAwake = bezierPath.playOnAwake;
     }
+
+    private void OnDestroy()
+    {
+        PTK_ModGameplayDataSync.Instance.gameEvents.OnGameEvent_RaceRestarted -= RaceRestarted;
+        PTK_ModGameplayDataSync.Instance.gameEvents.OnGameEvent_RaceTimerStart -= RaceTimerStart;
+    }
+
+    private void RaceRestarted()
+    {
+        if (bezierPath == null)
+            return;
+
+        if(bWasPlayOnAwake == true)
+        {
+            bezierPath.PlayPath(bezierPath.fBezierSpeed, false, true);
+            bezierPath.PausePath();
+        }
+    }
+
+    private void RaceTimerStart()
+    {
+        if (bezierPath == null)
+            return;
+
+        // play again to ensure events at 0 are triggered when match started
+        bezierPath.PlayPath(bezierPath.fBezierSpeed, false, true);
+    }
+
 
     // Update is called once per frame
     void Update()
